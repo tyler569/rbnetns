42c490f9c6a69ca9b1173efd8a58655bb0949e64
Tyler Philbrick <t5j6p9@gmail.com>
2020-02-22 09:02:19 -0800

    Delete netaddr dependancy

--- Gemfile
+++ Gemfile
@@ -4,7 +4,3 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "bundler", require: false
-# gem "irb", require: false
-
-gem "netaddr"
--- manage.rb
+++ manage.rb
@@ -1,6 +1,25 @@
 #!/usr/bin/env ruby
 
-require 'netaddr'
+require 'ipaddr'
+
+class IPAddr
+  def succ_subnet
+    n = self.dup
+    n.set(self.to_i + (2 << self.prefix-1))
+  end
+
+  def +(i)
+    self.dup.set(self.to_i + i)
+  end
+
+  def -(i)
+    self.dup.set(self.to_i - i)
+  end
+
+  def to_sub
+    self.to_s + "/" + self.prefix.to_s
+  end
+end
 
 class Netns
   attr_reader :name, :lo
@@ -37,12 +56,17 @@ class Netns
 
   def set_lo(net)
     @lo = net
-    exec "ip addr add #{net.nth(3).to_s}/64 dev lo"
+    exec "ip addr add #{(net + 2).to_sub} dev lo"
     exec "ip link set lo up"
   end
 
   def route(to, via)
-    exec "ip route add #{to.to_s} via #{via.to_s}"
+    exec "ip route add #{to.to_sub} via #{via.to_s}"
+  end
+
+  def enable_forwarding
+    exec "sysctl net.ipv6.conf.all.forwarding = 1"
+    exec "sysctl net.ipv4.ip_forward = 1"
   end
 
   def destroy
@@ -63,7 +87,7 @@ end
 class Link
   attr_reader :nss, :net, :num
 
-  @@base = NetAddr::IPv6Net.parse("4::/64")
+  @@base = IPAddr.new "4::/64"
   @@linknum = 0
 
   def initialize(*nss, **args)
@@ -71,7 +95,7 @@ class Link
     @num = args[:num] || @@linknum
     @nss = nss
 
-    @@base = @@base.next_sib
+    @@base = @@base.succ_subnet
     @@linknum += 1
 
     raw_setup
@@ -83,20 +107,8 @@ class Link
     nss.map{ |n| "v-#{@num}-#{n.name}" }.reverse
   end
 
-  def myip(ns)
-    if ns == nss[0]
-      net.nth(2)
-    else
-      net.nth(1)
-    end
-  end
-
-  def theirip(ns)
-    if ns == nss[0]
-      net.nth(1)
-    else
-      net.nth(2)
-    end
+  def ipof(ns)
+    (net + (nss.index(ns)+1)) if nss.include? ns
   end
 
   def destroy
@@ -109,7 +121,7 @@ class Link
     `ip link add #{names[0]} type veth peer name #{names[1]}`
     (0..1).each do |n|
       `ip link set #{names[n]} netns #{nss[n].name}` unless nss[n].is_default?
-      nss[n].exec "ip addr add #{net.nth(n+1).to_s}/64 dev #{names[n]}"
+      nss[n].exec "ip addr add #{(net + (n+1)).to_sub} dev #{names[n]}"
     end
     (0..1).each do |n|
       nss[n].exec "ip link set #{names[n]} up"
@@ -123,12 +135,21 @@ end
 
 default = Netns.new "default"
 blue = Netns.new "blue"
+red = Netns.new "red"
 
-all = NetAddr::IPv4Net.parse("0.0.0.0/0")
-all6 = NetAddr::IPv6Net.parse("::/0")
+blue.enable_forwarding
+red.enable_forwarding
 
-blue.set_lo(NetAddr::IPv6Net.parse("5::/64"))
+all = IPAddr.new "0.0.0.0/0"
+all6 = IPAddr.new "::/0"
+
+all_los = IPAddr.new "5::/16"
+
+blue.set_lo(IPAddr.new "5::/64")
+red.set_lo(IPAddr.new "5:1::/64")
+
 link = default.link_to blue
-blue.route(all6, link.myip(blue))
-default.route(blue.lo, link.myip(default))
+default.route(all_los, link.ipof(blue))
+
+red.link_to blue
 
